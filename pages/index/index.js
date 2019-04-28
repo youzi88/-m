b61ad4d731ea3ba722c8ee732c9425efa04cdcd3
youzi88 <1980491267@qq.com>
2019-04-28 21:37:18 +0800

    weather-miniapp

---
+++ pages/index/index.js
@@ -0,0 +1,16 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    temp:"4",
+    low:"-1℃",
+    high:"10℃",
+    type:"晴",
+    city:"北京",
+    week:"星期二",
+    weather:"无持续风行，微风"
+    
+  }
+})
